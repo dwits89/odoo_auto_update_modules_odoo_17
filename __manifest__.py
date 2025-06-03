ce1daef44f8e8342be0ef5d844f27e69d37bdcea
DWITS <lobo.digital.web@gmail.com>
2025-06-03 17:18:31 +0200

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Auto Update Modules Odoo 17',
+    'version': '17.0.1.0.0',
+    'summary': 'Actualiza automáticamente la lista de módulos al crear una base de datos',
+    'category': 'Tools',
+    'author': 'Digital Wolf IT Solutions',
+    'license': 'AGPL-3',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+}
